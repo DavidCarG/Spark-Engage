febd9d99833e2873006d1d4abb880a3adfa81e2a
Carlos Roberto <gonhiatsu@outlook.com>
2024-05-02 20:44:30 -0600

    This is a demo

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,6 +7,10 @@ export default function Home() {
         <li>Item 1</li>
         <li>Item 2</li>
         <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+        <li>Item 6</li>
+        <li>Item 7</li>
       </ul>
     </main>
   );
