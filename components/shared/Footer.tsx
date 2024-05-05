afef9d4e6766215dd391053cf03a18b3c838ca37
DavidC <davidcardenasgonz@gmail.com>
2024-05-05 10:47:17 -0600

    Fix: className typo

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -4,16 +4,20 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer classsName="">
-      <div classsName="flex-center wrapped flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+    <footer className="">
+      <div className="flex-center wrapped flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
         <Link href="/">
-          <Image src="/assets/images/logo.svg" alt="Logo" width={128} height={38}/>   
+          <Image
+            src="/assets/images/logo.svg"
+            alt="Logo"
+            width={128}
+            height={38}
+          />
         </Link>
         <p>2024 Evently. All Rights reserved</p>
       </div>
-       
     </footer>
-  )
+  );
 };
 
 export default Footer;
