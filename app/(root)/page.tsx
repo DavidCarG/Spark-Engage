bac21152c646316465d3bda3625ab2425b702d38
Carlos Roberto <gonhiatsu@outlook.com>
2024-05-04 21:57:24 -0600

    Finished the footer and the main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,36 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-    <main>
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-        <li>Item 4</li>
-        <li>Item 5</li>
-        <li>Item 6</li>
-        <li>Item 7</li>
-        <li>Item 8</li>
-        <li>Item 9</li>
-      </ul>
-    </main>
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrpped grid gird-cols-1 gap-5 md:grid-cols-2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">Host,Connect Celebrate: Your Events, Our Plataform!</h1>
+            <p className="p-regular-20 md:p-regular-24">Book and learn 
+            helpful tips from 3,168+ mentors in world-class companies 
+            with our global community.</p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events" >
+              Explore Now
+              </Link>
+            </Button>
+            
+          </div>
+          <Image src="/assets/images/hero.svg" alt="Hero" width={1000} height={1000} className="max-h-[70vh] object-contain object-center
+          2xl:max-h-[50vh]"/>
+        </div>
+      </section>
+      <section id="events" className="wrapper my-8 flex flex-col gap-89 md:gap-12">
+        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          Search
+          CategoryFilter
+        </div>
+      </section>
+    </>
   );
 }
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer classsName="">
+      <div classsName="flex-center wrapped flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image src="/assets/images/logo.svg" alt="Logo" width={128} height={38}/>   
+        </Link>
+        <p>2024 Evently. All Rights reserved</p>
+      </div>
+       
+    </footer>
+  )
 };
 
 export default Footer;
