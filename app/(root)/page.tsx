9f23e80e355231b7d16b41ea37d97b189e59e098
DavidC <davidcardenasgonz@gmail.com>
2024-05-05 10:53:56 -0600

    Fix: revert main branch to a clean state

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <main>
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-        <li>Item 4</li>
-        <li>Item 5</li>
-        <li>Item 6</li>
-        <li>Item 7</li>
-        <li>Item 8</li>
-        <li>Item 9</li>
-      </ul>
-    </main>
-  );
+  return <main></main>;
 }
