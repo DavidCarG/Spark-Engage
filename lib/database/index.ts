8a21978efe950ce15961eaf41c2838b104b7bcb5
DavidC <davidcardenasgonz@gmail.com>
2024-05-06 22:26:06 -0600

    Add MongoDB and Mongoose dependencies, and create database connection file

---
+++ lib/database/index.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cached = (global as any).mongoose || { con: null, promise: null };
+
+export const connectToDatabase = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!MONGODB_URI) {
+    throw new Error("MONGODB_URI is missing");
+  }
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "sparkengagedb",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
